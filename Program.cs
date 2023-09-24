dbfbb1c978d889b0890711398a9d59e1a420d6aa
Bietood <gabriel.duarte69@etec.sp.gov.br>
2023-09-24 16:51:54 -0300

    Exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+double consumo, HorasUsoDia;
+decimal Custo, estimativaMes;
+Console.WriteLine("---Custo de Energia---\n");
+Console.Write("Consumo do aparelho (em kWh/mês).:");
+consumo = Convert.ToDouble(Console.ReadLine());
+Console.Write("Horas de uso por dia.:");
+HorasUsoDia = Convert.ToDouble(Console.ReadLine());
+Console.Write("Custo da Energia (em R$/kWh).:");
+Custo = Convert.ToDecimal(Console.ReadLine());
+estimativaMes = Convert.ToDecimal(consumo * HorasUsoDia) * Custo;
+Console.WriteLine($"\nCusto estimado: {estimativaMes:C}");
